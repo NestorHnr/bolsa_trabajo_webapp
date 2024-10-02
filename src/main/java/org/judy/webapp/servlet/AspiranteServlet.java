94ff825d30485122c3f102f726ff6f6d383d1723
Nestor Heredia <nestornhr27@hotmail.com>
2024-10-01 22:05:40 -0500

    agregar Hola Mundo

---
+++ src/main/java/org/judy/webapp/servlet/AspiranteServlet.java
@@ -0,0 +1,11 @@
+package org.judy.webapp.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet
+public class AspiranteServlet extends HttpServlet {
+
+
+
+}
